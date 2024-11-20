ab8bc94c3e7934709180500cb1e614b346413b67
Gaby0618 <limainaya22@gmail.com>
2024-11-20 15:35:29 -0400

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'É uma linguagem de marcação usada para criar e estruturar páginas da web'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual o valor de pi (π) ?',
+    'O valor de pi (π) é 3,14'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritimo?',
+    'Uma sequência de passos que descrevem como resolver um problema, ou realizar uma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz escola em Inglês?',
+    'Escola em inglês é school'
+)
